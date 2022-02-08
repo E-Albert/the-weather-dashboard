4e66f4b0a17cd0976074fb1118487ae1c10f77d8
E-Albert <magicspoon123@gmail.com>
2022-02-08 10:47:16 -0500

    Got the geoApi to work

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ utilize weather api
 use information from api to build the display
 dyanmically change depending on the city*/
 
-//global variables
-let searchHistory = [];
-let apiKey = 'e681224f251edf9fe2b18dfc26040eac';
-let weatherApi = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}';
-let geoApi = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+
 
 //DOM elements
 let srchBtnEl = document.querySelector('.searchButton');
@@ -17,20 +13,33 @@ let weatherContainerEl = document.querySelector('.currentWeatherContainer');
 let cardContainerEl = document.querySelector('.card');
 let formEl = document.querySelector('#form');
 
+//global variables
+let searchHistory = [];
+let apiKey = 'e681224f251edf9fe2b18dfc26040eac';
+let weatherApi = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}`;
+// let geoApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${apiKey}`;
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
+//DOM elements
+// let srchBtnEl = document.querySelector('.searchButton');
+// let srchBarEl = document.querySelector('#searchBar');
+// let weatherContainerEl = document.querySelector('.currentWeatherContainer');
+// let cardContainerEl = document.querySelector('.card');
+// let formEl = document.querySelector('#form');
 
-    var city = srchBarEl.value.trim();
 
-    if (city) {
-        // getUserRepos(username);
+let getCityCords = function (city) {
+    console.log(`Getting coordinates for: ${city}`)
+    let geoApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${apiKey}`;
 
-        srchBarEl.value = '';
-    } else {
-        alert('Please enter a city');
-    }
-};
+    fetch(geoApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
+}
 
 
 
@@ -43,7 +52,9 @@ var formSubmitHandler = function (event) {
 //         console.log(data);
 //     })
 
-
+const getWeather = function (lat, lon) {
+    
+}
 
 
 
@@ -55,5 +66,22 @@ var formSubmitHandler = function (event) {
 //     .then(function (data) {
 //         console.log(data);
 //     })
+
+
+let formSubmitHandler = function (event) {
+    event.preventDefault(); 
+    console.log('Submitting form')
+
+    let city = srchBarEl.value.trim();
+    console.log(city);
+    if (city) {
+
+        getCityCords(city);
+
+        srchBarEl.value = '';
+    } else {
+        alert('Please enter a city');
+    }
+};
 
 formEl.addEventListener('submit', formSubmitHandler);
